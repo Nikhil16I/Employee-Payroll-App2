f93ce2db1b65f6a641646efb98461d609eec4d90
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-02-07 16:04:11 +0530

    Created Rest Controller  and tested all HTTP methods

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -1,9 +1,9 @@
 package com.bridgelabz.employeepayrollapp.controller;
 
+import com.bridgelabz.employeepayrollapp.dto.EmploeePayrollDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employeepayrollservice")
@@ -12,5 +12,21 @@ public class EmployeePayrollController {
     @RequestMapping(value = {"","/","/get","home"})
 public ResponseEntity<String>getEmployeePayrollData(){
 return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
+ }
+ @GetMapping("/get/{empId}")
+ public ResponseEntity<String>getEmployeePayrollData(@PathVariable("empId")int empId){
+     return new ResponseEntity<String>("Get Call Success for Id:"+empId,HttpStatus.OK);
+}
+@PostMapping("/create")
+public ResponseEntity<String> addEmployeePayrollData(@RequestBody EmploeePayrollDTO emploeePayrollDTO){
+return  new ResponseEntity<String>("Created Employee Payroll Data for:"+emploeePayrollDTO,HttpStatus.OK);
+}
+@PutMapping("/update")
+public ResponseEntity<String>updateEmployeePayrollData(@RequestBody EmploeePayrollDTO emploeePayrollDTO){
+return new ResponseEntity<String>("Updated Employee Payroll Data for :"+emploeePayrollDTO,HttpStatus.OK);
+}
+@DeleteMapping("/Delete/{empId}")
+public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId){
+return new ResponseEntity<String>("Delete Call Success for Id :"+empId,HttpStatus.OK);
  }
 }
